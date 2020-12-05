78f98c182651185409c1e10837ccee7e60be5591
cruzoanton <74861347+cruzoanton@users.noreply.github.com>
2020-12-05 13:51:04 +0200

    commit7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println("Привет");
         System.out.println("Привет");
         System.out.println("Привет");
+        System.out.println("Привет");
+        System.out.println("Привет");
 
     }
 }
